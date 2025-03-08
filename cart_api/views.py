f3b7daed80a02782f890221eec3dd415e19d0192
gyanu <gyanuchauhan765@gmail.com>
2025-03-08 10:35:39 +0530

    basic api created

--- cart_api/views.py
+++ cart_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 # Create your views here.
+
+class HomeView(APIView):
+
+    @staticmethod
+    def get(request):
+        return Response({"success":True, "message":"Welcome to home page", "data":[]}, status=status.HTTP_200_OK)
